ab6cb480d498e557508890e48367c2f6ef6b96e9
PanGeun <lifebook0809@gmail.com>
2020-08-14 21:30:37 +0900

    [ADD] Setting 화면 및 테스트 tableView 생성

---
+++ poochaser/poochaser/Secene/Main/ViewController/SettingViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SettingViewController.swift
+//  poochaser
+//
+//  Created by 판근 on 2020/08/14.
+//  Copyright © 2020 Jeonpangeun. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
+    let test: [String] = ["SETTING", "MAIL TO DEVELOPER", "POO CHASER INSTAGRAM", "INTRODUCE DEVELOPER", "LOGOUT", "회원 탈퇴"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+extension SettingViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return test.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "settingCell", for: indexPath)
+        cell.textLabel?.text = "\(test[indexPath.row])"
+        return cell
+    }
+}
