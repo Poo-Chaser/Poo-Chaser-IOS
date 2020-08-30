3ca6896ec0e586066a49eabbc5aa331ff5a75db6
PanGeun <lifebook0809@gmail.com>
2020-08-31 00:50:48 +0900

    [ADD] Register 추가

--- poochaser/poochaser/Secene/Register/ViewController/RegisterController.swift
+++ poochaser/poochaser/Secene/Register/ViewController/RegisterController.swift
@@ -7,16 +7,35 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class RegisterController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var checkPasswordTextField: UITextField!
+    
+    @IBAction func registerPressed(_ sender: UIButton) {
+        
+        if let email = emailTextField.text, let password = passwordTextField.text, let checkPassword = checkPasswordTextField.text {
+            if password == checkPassword {
+                Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                   if let e = error {
+                       print(e.localizedDescription)
+                   } else {
+                       // Navigate to the Chat ViewController
+                       self.performSegue(withIdentifier: "Login", sender: self)
+                   }
+                }
+            }
+            
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
-
-
 }
 
 
