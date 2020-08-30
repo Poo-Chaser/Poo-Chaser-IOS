0d621fc6a989db0d4d07077939a1ebaadf3ab2e9
PanGeun <lifebook0809@gmail.com>
2020-08-31 01:12:18 +0900

    [ADD] Login 추가

--- poochaser/poochaser/Secene/Login/ViewController/LoginViewController.swift
+++ poochaser/poochaser/Secene/Login/ViewController/LoginViewController.swift
@@ -7,11 +7,29 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBAction func LoginBtn(_ sender: UIButton) {
+        if let email = emailTextField.text, let password = passwordTextField.text {
+            Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
+                if let e = error {
+                    print(e)
+                } else {
+                    self!.performSegue(withIdentifier: "LoginToMain", sender: self)
+                }
+            }
+        }
+        
+        
+    }
+    
     @IBAction func RegisterBtn(_ sender: Any) {
-        print("Register!")
+        print("To Register!")
     }
     
     override func viewDidLoad() {
